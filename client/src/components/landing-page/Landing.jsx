f8b5a6e5a4d2a0cbea7b80611f2f12d68b30d40e
EzequielRamirez <ramirez.ezequielagustin@gmail.com>
2022-11-03 18:00:28 -0300

    change tittle(client/src/components/Landing)

--- client/src/components/landing-page/Landing.jsx
+++ client/src/components/landing-page/Landing.jsx
@@ -9,7 +9,7 @@ const Landing = () => {
 
 	return (
 		<LandingPage>
-			<Button onClick={() => navigate("/")}>LET'S COOK MASTER CHEFF</Button>
+			<Button onClick={() => navigate("/")}>LET'S COOK MASTER CHEFFff</Button>
 		</LandingPage>
 	);
 };
